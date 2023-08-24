5317734154c4281a9762b3272266c81d9c44f052
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2023-08-24 18:44:32 -0300

    First commit

---
+++ src/main/java/br/g12/duque/antonio/logica2/Logica2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.g12.duque.antonio.logica2;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Logica2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
